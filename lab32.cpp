ce9f54f82f2fb5f6198661ecf17ab1b9429e70c6
ambucha <ambuchatskiy@gmail.com>
2025-11-14 09:08:07 -0800

    milestone 1 just wanted a deque with two items so i guess its done here

--- lab32.cpp
+++ lab32.cpp
@@ -4,7 +4,22 @@
 #include <iostream>
 #include <deque>
 #include "Car.h"
+using namespace std;
+
+// create the const variables, such as starting cars in toll ammount, also probabilities
+const int STARTING_SIZE = 2;
+const int PAYS = 55;
+const int JOINS = 45;
+
 
 int main(){
-    
+    // start by declaring a deque that will store cars, initialize to size 2
+    deque<Car> toll(STARTING_SIZE);
+
+    // test to see if the cars are in the toll
+    for(auto car : toll){
+        car.print();
+    }
+
+    return 0;
 }
